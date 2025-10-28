fabdbbbd53e163ff4758923a3fc7dc6e8b2e8ed3
Daniel Enubiaka <danielenubiakamail@gmail.com>
2025-10-28 13:50:08 +0000

    feat: Remove unused files and update popup layout in Hide Comments extension

--- run.sh
+++ run.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
 
-if [ "$1" = "--delete-orphans" ] || [ "$2" = "--delete-orphans" ]; then
+# Parse arguments
+delete_orphans=false
+use_self=false
+custom_name=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --delete-orphans)
+            delete_orphans=true
+            shift
+            ;;
+        --self)
+            use_self=true
+            shift
+            ;;
+        --to)
+            custom_name="$2"
+            shift 2
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+if [ "$delete_orphans" = true ]; then
     rm -rf ./output/*
 fi
 
 mkdir -p ./output
 current_date=$(date +"%Y-%m-%d_%H-%M-%S")
-archive_name="archive_$current_date"
 
-if [ "$1" = "self" ] || [ "$2" = "self" ]; then
-    tar -cJf ./output/$archive_name.tar.xz --transform="s,^\./,$archive_name/," ./
+if [ -n "$custom_name" ]; then
+    archive_name="$custom_name"
 else
-    tar -cJf ./output/$archive_name.tar.xz ./
+    archive_name="archive_$current_date"
+fi
+
+if [ "$use_self" = true ]; then
+    tar -cJf ./output/$archive_name.tar.xz --transform="s,^\./,$archive_name/," --exclude='./output' --exclude-vcs ./
+else
+    tar -cJf ./output/$archive_name.tar.xz --exclude='./output' --exclude-vcs ./
 fi
